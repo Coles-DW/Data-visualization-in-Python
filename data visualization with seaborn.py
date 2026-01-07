a7fb4d6231c0d5eda9066072447531da71801620
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2026-01-07 15:06:04 -0700

    Update data visualization with seaborn.py

--- data visualization with seaborn.py
+++ data visualization with seaborn.py
@@ -469,3 +469,17 @@ sns.catplot(x="romantic", y="absences",
 # Show plot
 plt.show()
 
+# Set the style to "whitegrid"
+sns.set_style("whitegrid")
+
+# Create a count plot of survey responses
+category_order = ["Never", "Rarely", "Sometimes", 
+                  "Often", "Always"]
+
+sns.catplot(x="Parents Advice", 
+            data=survey_data, 
+            kind="count", 
+            order=category_order)
+
+# Show plot
+plt.show()
